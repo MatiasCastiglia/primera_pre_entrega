c831dba596a02be7e076a3094f6239d5f537c269
MatiasCastiglia <88591540+MatiasCastiglia@users.noreply.github.com>
2023-06-13 19:34:22 -0300

    Add files via upload

--- primera_pre_entrega.js
+++ primera_pre_entrega.js
@@ -10,7 +10,7 @@ saludoUsuario()
 let mensaje = prompt("¿Deseas comprar algún producto? (s-si)");
 let total = 0;
 
-while (mensaje.toLowerCase() == "s") {
+while (mensaje.toLowerCase() == "si"){
   let producto = prompt(
     "1 - Buzo de frisa $4000\n2 - Remera de algodón $2500\n3 - Jean Elastizado $5000\n4 - Remera manga larga $3000\n5 - Campera de frisa $6000"
   );
@@ -49,6 +49,51 @@ function incrementarTotal(precio) {
   total = total + precio;
   alert("Precio a pagar hasta el momento: $" + total);
 }
+
+// Mi segunda pre entrega 
+
+class Producto{
+  constructor(nombre,precio,talle){
+      this.nombre = nombre;
+      this.precio = precio
+      this.talle = talle
+  }
+}
+
+const productos = []
+
+function agregarProducto(){
+  let ingresarNombre = prompt("Ingrese el nombre del producto").toLowerCase()
+  let ingresarPrecio = parseFloat(prompt("Ingrese el precio del producto"))
+  let ingresarTalle = prompt("Ingrese el talle")
+  let nuevoProducto = new Producto(ingresarNombre,ingresarPrecio,ingresarTalle)
+  productos.push(nuevoProducto)
+  console.log(productos) 
+}
+
+function filtrarProductos(array){
+  let buscarProducto = prompt("Ingrese el nombre del producto").toLowerCase()
+  const productosEncontrados = array.filter((elemento)=>elemento.nombre === buscarProducto)
+  if(productosEncontrados.length === 0){
+      alert("Producto no encontrado")
+  }
+  console.log("Productos filtrados:")
+  console.log(productosEncontrados)
+}
+
+let ingresarDato = prompt("¿Qué deseas hacer? \n1)Agregar Producto \n2)Filtrar Productos \n3)Salir")
+
+while(ingresarDato !== "3"){
+  if(ingresarDato==="1"){
+      agregarProducto()
+  }else if(ingresarDato ==="2" && productos.length > 0){
+      filtrarProductos(productos)
+  }else{
+      alert("Opción no valida")
+  }
+  ingresarDato = prompt("¿Qué deseas hacer? \n1)Agregar Producto  \n2)Filtrar Productos \n3)Salir")
+}
+
 
 
 
